e780095363eddc385725be0977fcff4086a3a683
Santiago MVO <santiago.mvao@gmail.com>
2020-12-06 20:32:00 -0600

    added header mobile display, added footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,8 @@ import React from "react";
 import "./style.sass";
 
 export default function Footer(){
-	return <footer id="footer">
-		
+	return <footer id="footer" className="flex f-jsb f-ac">
+		<small>Santiago Martinez Vara Ortiz</small>
+		<small><i className="icon">copyright</i> 07/12/2020</small>
 	</footer>
 }
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,8 +7,11 @@ export default function Header(){
 	return <header id="header" className="flex f-jsb f-ac dp-3">
 		<a className="h1" href="/">Marvel Heroes</a>
 		<div className="links">
+			<NavLink to="/heroes" activeClassName="b" exact>Heroes</NavLink>
 			<NavLink to="/comics" activeClassName="b" exact>Comics</NavLink>
-			<NavLink to="/saved"  activeClassName="b" exact>Saved</NavLink>
+			<NavLink to="/events" activeClassName="b" exact>Events</NavLink>
+			{/* <NavLink to="/series" activeClassName="b" exact>Series</NavLink> */}
+			<NavLink to="/saved"  activeClassName="b" exact>Saved Heroes</NavLink>
 		</div>
 	</header>
 }
